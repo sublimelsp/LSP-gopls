88500887843cbb7f6d7edd5561d66f9b899ac8cf
TheSecEng <zachary.schulze@gmail.com>
2022-04-13 21:50:32 -0500

    update to latest gopls

--- plugin.py
+++ plugin.py
@@ -16,7 +16,7 @@ Current version of gopls that the plugin installs
 Review gopls settings when updating TAG to see if
 new settings exist
 '''
-TAG = '0.8.1'
+TAG = '0.8.3'
 GOPLS_BASE_URL = 'golang.org/x/tools/gopls@v{tag}'
 
 RE_VER = re.compile(r'go(\d+)\.(\d+)(?:\.(\d+))?')
